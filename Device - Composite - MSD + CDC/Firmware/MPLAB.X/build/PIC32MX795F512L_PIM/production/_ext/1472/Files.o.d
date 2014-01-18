42c2c4cdf73fab354194e9d024fe2277e18faaa9
soerSD <soerSD@yandex.ru>
2014-01-18 16:28:53 +0300

    ДОБАВЛЕН ОРИНИНАЛЬНЫЙ ПРОЕКТ ИЗ СТАНДАРТНОЙ БИБЛИОТЕКИ.

---
+++ Device - Composite - MSD + CDC/Firmware/MPLAB.X/build/PIC32MX795F512L_PIM/production/_ext/1472/Files.o.d
@@ -0,0 +1,9 @@
+build/PIC32MX795F512L_PIM/production/_ext/1472/Files.o:  \
+../Files.c  \
+ ../../../../Microchip/Include/Compiler.h  \
+../FSconfig.h  \
+ ../HardwareProfile.h  \
+../HardwareProfile\ -\ PIC32MX795F512L\ PIM.h  \
+ ../../../../Microchip/Include/MDD\ File\ System/Internal\ Flash.h  \
+ ../FSconfig.h  \
+../../../../Microchip/Include/MDD\ File\ System/FSDefs.h 
